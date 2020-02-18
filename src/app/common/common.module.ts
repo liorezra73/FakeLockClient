67abec1479d562bb54f585c15261561ce3c41b37
liorezra73 <53858691+liorezra73@users.noreply.github.com>
2020-02-18 11:52:05 +0200

    loginFix

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormTextInputComponent } from "./components/form-text-input/form-text-input.component";
-<<<<<<< HEAD
 import { FormPasswordInputComponent } from './components/form-password-input/form-password-input.component';
-
-@NgModule({
-  declarations: [FormTextInputComponent, FormPasswordInputComponent],
-  imports: [CommonModule],
-  exports: [FormTextInputComponent, FormPasswordInputComponent]
-=======
 import { FormInputErrorComponent } from './components/form-input-error/form-input-error.component';
 
 @NgModule({
-  declarations: [FormTextInputComponent, FormInputErrorComponent],
+  declarations: [FormTextInputComponent, FormPasswordInputComponent,FormInputErrorComponent],
   imports: [CommonModule],
-  exports: [FormTextInputComponent,FormInputErrorComponent]
->>>>>>> formTextInputFix
+  exports: [FormTextInputComponent, FormPasswordInputComponent,FormInputErrorComponent]
 })
 export class AppCommonModule {}
