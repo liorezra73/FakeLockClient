a1b1fd4c120ae77c680bcd5476f00eab5f0aee6a
konchashlomi <53858749+konchashlomi@users.noreply.github.com>
2020-02-20 10:03:35 +0200

    DeletedAotuComplete

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TagInputComponent } from './components/tag-input/tag-input.component';
 import { UsertagInputComponent } from './components/usertag-input/usertag-input.component';
-import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 
 @NgModule({
   declarations: [TagInputComponent, UsertagInputComponent],
   imports: [CommonModule],
-  exports: [TagInputComponent,UsertagInputComponent,AutocompleteLibModule]
+  exports: [TagInputComponent,UsertagInputComponent]
 })
 export class AppSharedModule {}
