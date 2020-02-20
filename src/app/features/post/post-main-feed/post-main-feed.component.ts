076dec1cc8719865a0668ddb2bf2587c08e8d0d4
konchashlomi <53858749+konchashlomi@users.noreply.github.com>
2020-02-20 10:56:42 +0200

    FeedPost

--- src/app/features/post/post-main-feed/post-main-feed.component.ts
+++ src/app/features/post/post-main-feed/post-main-feed.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PostService } from "src/app/common/services/post-service.service";
+import { IPostService } from "src/app/common/intefaces/post-service.inteface";
+import { Post } from 'src/app/common/models/post';
 
 @Component({
-  selector: 'app-post-main-feed',
-  templateUrl: './post-main-feed.component.html',
-  styleUrls: ['./post-main-feed.component.css']
+  selector: "app-post-main-feed",
+  templateUrl: "./post-main-feed.component.html",
+  styleUrls: ["./post-main-feed.component.css"]
 })
 export class PostMainFeedComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  postService: IPostService;
+  posts: Post[];
+  constructor(service: PostService) {
+    this.postService = service;
   }
 
+  ngOnInit() {}
+
+  getPost(){
+    this.postService.getPostsOrderByDates();
+  }
 }
